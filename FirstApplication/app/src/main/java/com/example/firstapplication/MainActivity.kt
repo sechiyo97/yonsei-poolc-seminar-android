27b0958b2a66973273200cfd9c286acde861206d
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-09-29 00:40:21 +0900

    show submit data on click

--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // show toast on click
+        // add on click event
         submit_button.setOnClickListener{
+            // show toast on click
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+
+            // show submit data
+            submit_datetime.text = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault()).format(Date())
+            submit_text.text = edit_text.text.toString()
         }
     }
 }
