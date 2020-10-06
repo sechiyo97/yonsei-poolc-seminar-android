a4318decdd44cf41fc0ad9b9a7c4ef8662997da2
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-10-07 03:16:45 +0900

    add fragment transaction onto button onclicks

--- FragmentExample/app/src/main/java/com/example/fragmentexample/MainActivity.kt
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.fragmentexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val firstFragment = FirstFragment()
+    val secondFragment = SecondFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        first_button.setOnClickListener{
+            changeFragment(firstFragment)
+        }
+
+        second_button.setOnClickListener{
+            changeFragment(secondFragment)
+        }
+
+        changeFragment(firstFragment)
+    }
+
+    fun changeFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content_view, fragment)
+            .commit();
     }
 }
