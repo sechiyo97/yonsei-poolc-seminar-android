a0fe6d7e168424db057c29c08234c3c0559af4a5
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-10-07 01:23:50 +0900

    get username by sharedpreferences in main activity

--- FirstApplication/app/src/main/java/com/example/firstapplication/RegisterActivity.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/RegisterActivity.kt
@@ -18,12 +18,12 @@ class RegisterActivity : AppCompatActivity() {
         edit_text.requestFocus()
     }
     fun saveUserName(){
-        val userName = getSharedPreferences("userName", MODE_PRIVATE)
+        val userName = getSharedPreferences("userInfo", MODE_PRIVATE)
         userName.edit().putString("user_name", edit_text.text.toString()).apply()
     }
     fun goToMainActivity(){
         val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("user_name", edit_text.text.toString())
+        //intent.putExtra("user_name", edit_text.text.toString())
         startActivity(intent) // 메인액티비티 실행
         finish() // 스플래시액티비티 종료
     }
