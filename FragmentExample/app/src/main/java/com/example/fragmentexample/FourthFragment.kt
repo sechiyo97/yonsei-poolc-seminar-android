d20f264ae5ac71dd5cdbed767534f29673870e76
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-10-07 04:32:57 +0900

    extend to four fragments, and change some layout styles

---
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/FourthFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FourthFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentView = inflater.inflate(R.layout.fragment_fourth, container, false)
+        return fragmentView.rootView
+    }
+}
--- FragmentExample/app/src/main/java/com/example/fragmentexample/MainActivity.kt
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -8,6 +8,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     val firstFragment = FirstFragment()
     val secondFragment = SecondFragment()
+    val thirdFragment = ThirdFragment()
+    val fourthFragment = FourthFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,14 @@ class MainActivity : AppCompatActivity() {
 
         second_button.setOnClickListener{
             changeFragment(secondFragment)
+        }
+
+        third_button.setOnClickListener{
+            changeFragment(thirdFragment)
+        }
+
+        fourth_button.setOnClickListener{
+            changeFragment(fourthFragment)
         }
 
         changeFragment(firstFragment)
