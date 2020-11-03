804d62195ac758403909c4cd2f78d8aacb502a4e
day.dream <day.dream@kakaocorp.com>
2020-11-03 20:23:08 +0900

    알고리즘 수정, 디데이 표시

--- ddaycounter/app/src/main/java/com/example/ddaycounter/MainActivity.kt
+++ ddaycounter/app/src/main/java/com/example/ddaycounter/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.ddaycounter
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,9 +23,6 @@ class MainActivity : AppCompatActivity() {
 
             datePickerDialog.setOnDateSetListener{
                 _, year, month, day ->
-                val dateString = "$year.$month.$day 부"
-                start_date_text.text = dateString
-
                 calculateAndDisplayResult(year, month, day)
             }
 
@@ -34,6 +31,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calculateAndDisplayResult(year:Int, month:Int, day:Int){
+        // set date text
+        val dateString = "$year.${month+1}.$day 부터"
+        start_date_text.text = dateString
 
+        // start
+        val startDateTime = Calendar.getInstance().apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month)
+            set(Calendar.DAY_OF_MONTH, day)
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+
+        // end
+        val endDateTime = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+
+        // calc
+        val pastDate =
+            ((endDateTime - startDateTime) / (24 * 60 * 60 * 1000))
+                .toInt() + 1
+
+        // update
+        d_day_text.text = "D+$pastDate"
     }
 }
