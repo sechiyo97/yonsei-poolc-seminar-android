be5ebe33d2eb53023cbdb2adfa2c2c82fe36f255
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-11-11 00:53:23 +0900

    adapter, viewholder, item_view 구현

---
+++ GalleryExampleApp/app/src/main/java/com/example/galleryexampleapp/ImageAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.galleryexampleapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter : RecyclerView.Adapter<ImageAdapter.MyViewHolder>(){
+    var imageResourceList : List<Int> = emptyList()
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val imageView = view.findViewById<ImageView>(R.id.image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  MyViewHolder {
+        // create a new view
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_image, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageResourceList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imageResourceList.size
+    }
+
+}
