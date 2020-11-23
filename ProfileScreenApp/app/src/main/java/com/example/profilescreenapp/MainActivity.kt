ed7deb103c21c1cbd4f4a4df97523bc5d7a59d67
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-11-23 23:09:05 +0900

    Adapter, LinearLayoutManager 설정하기

--- ProfileScreenApp/app/src/main/java/com/example/profilescreenapp/MainActivity.kt
+++ ProfileScreenApp/app/src/main/java/com/example/profilescreenapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.profilescreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        recycler_profile.layoutManager = LinearLayoutManager(this)
+
+        val adapter = ProfileAdapter()
+        recycler_profile.adapter = adapter
+
+        // set data
+        adapter.profileList = listOf(
+            Profile(R.drawable.ic_launcher_background, "이세희", "배고파"),
+            Profile(R.drawable.ic_launcher_background, "홍길동", "나는 길동이야"),
+            Profile(R.drawable.ic_launcher_background, "뀨", "안뇽"),
+            Profile(R.drawable.ic_launcher_foreground, "사람", "어떤 사람"),
+            Profile(R.drawable.ic_launcher_foreground, "사람", "어떤 사람"),
+            Profile(R.drawable.ic_launcher_foreground, "사람", "어떤 사람"),
+            Profile(R.drawable.ic_launcher_foreground, "사람", "어떤 사람"),
+            Profile(R.drawable.ic_launcher_foreground, "사람", "어떤 사람"),
+            Profile(R.drawable.ic_launcher_foreground, "마지막 사람", "마지막 사람"),
+        )
+        adapter.notifyDataSetChanged()
     }
 }
--- ProfileScreenApp/app/src/main/java/com/example/profilescreenapp/ProfileAdapter.kt
+++ ProfileScreenApp/app/src/main/java/com/example/profilescreenapp/ProfileAdapter.kt
@@ -1,2 +1,35 @@
 package com.example.profilescreenapp
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
+
+    // 데이터 리스트
+    var profileList : List<Profile> = listOf()
+
+    inner class ProfileViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val imageProfile = itemView.findViewById<ImageView>(R.id.image_profile)
+        val textName = itemView.findViewById<TextView>(R.id.text_name)
+        val textMessage = itemView.findViewById<TextView>(R.id.text_message)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_profile, parent,false)
+        return ProfileViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+        holder.imageProfile.setImageResource(profileList[position].image)
+        holder.textName.text = profileList[position].name
+        holder.textMessage.text = profileList[position].message
+    }
+
+    override fun getItemCount(): Int {
+        return profileList.size
+    }
+}
