d5c7acce0368783e5eec8bb6e999f28a74c94082
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-11-23 23:12:29 +0900

    ChatActivity 생성 및등록

---
+++ ProfileScreenApp/app/src/main/java/com/example/profilescreenapp/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.example.profilescreenapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+
+}
